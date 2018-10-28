48fbf288389176ee60fbea055da1a0334eb99ad9
embg <elliot.gorokhovsky@gmail.com>
2018-10-27 17:16:59 -0700

    Add pip and venv to the script

--- depth_experiment/depth_experiment.sh
+++ depth_experiment/depth_experiment.sh
@@ -1,3 +1,7 @@
+python3 -m venv .
+. venv/bin/activate
+pip install -r requirements.txt
+
 for f in $(find . -name '*.cnf'); do
-    maplesat f | python tabulator.py > depth_experiment.csv
+    maplesat f | python depth_experiment.py > depth_experiment.csv
 done
