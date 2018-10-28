549d85362664cb3a9b8869437df43b05a41e5a3a
embg <elliot.gorokhovsky@gmail.com>
2018-10-27 18:27:36 -0700

    Python script tested and working!

--- depth_experiment/depth_experiment.py
+++ depth_experiment/depth_experiment.py
@@ -1,5 +1,18 @@
-import sys
-if sys.version_info[0] < 3:
-    print("Use Python 3!")
+import sys, npstreams
+import numpy as np
+
+cleaned = (line.rstrip().split() for line in sys.stdin)
+filtered = (map(int, line[1:]) for line in cleaned if line[0] == 'NFNT')
+stream = ((NF, NF / NT) for NF, NT in filtered)
+# Correct up to here
+
+stream_for_avgs, stream_for_stds = npstreams.itercopy(stream)
+iavgs = npstreams.imean(stream_for_avgs, axis=1)
+istds = npstreams.istd(stream_for_stds, axis=1)
+
+avgs, stds = npstreams.last(zip(iavgs, istds))
+print(*list(avgs), *list(stds))
+# Incorrect
+
 
 
--- depth_experiment/depth_experiment.sh
+++ depth_experiment/depth_experiment.sh
@@ -1,7 +1,15 @@
-python3 -m venv .
-. venv/bin/activate
-pip install -r requirements.txt
+# Build venv
+virtualenv -p /usr/bin/python3 venv;
+. venv/bin/activate;
+pip3 install -r requirements.txt;
 
+# Run experiment
+echo "RAW_AVG, FRAC_AVG, RAW_STD, FRAC_STD" > depth_experiment.csv;
+python depth_experiment.py < experiment.test >> depth_experiment.csv;
 for f in $(find . -name '*.cnf'); do
-    maplesat f | python depth_experiment.py > depth_experiment.csv
+    maplesat f | python depth_experiment.py >> depth_experiment.csv;
 done
+
+# Destroy venv
+. deactivate;
+rm -r venv;
